279cb309b5d6c8bd5e5aafea1d83374337e33934
PepGames <85426927+PepGames@users.noreply.github.com>
2025-07-25 20:14:07 -0400

    update description

--- build_with_java21.bat
+++ build_with_java21.bat
@@ -1,6 +1,6 @@
 @echo off
 set JAVA_HOME=C:\Program Files\Java\jdk-21
 set PATH=%JAVA_HOME%\bin;%PATH%
-cd /d "C:\Users\Pep\Desktop\Game Dev\Minecraft\Fabric\moblist-template-1.21.4"
+cd /d "C:\Repos\MobList"
 gradlew build
 pause
